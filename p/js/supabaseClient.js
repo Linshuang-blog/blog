dd9a581a96469090779ea4573a85a93a6ff02b63
lin shuang <lran94@stu.jjc.edu>
2025-07-26 09:58:32 +0800

    Update supabaseClient.js

--- p/js/supabaseClient.js
+++ p/js/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // Supabase 项目配置信息
-const SUPABASE_URL = 'https://klyrasrqgxijwrxuoevj.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtseXJhc3JxZ3hpandyeHVvZXZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4ODUyMzcsImV4cCI6MjA2ODQ2MTIzN30.qjeTrLp_QquSwvF09HrrQd-stPtgu6H51-Zdb4JUeSM'
+const SUPABASE_URL = 'https://xhnsrdrtgyjlhdcrkedh.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobnNyZHJ0Z3lqbGhkY3JrZWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0OTA1NzcsImV4cCI6MjA2OTA2NjU3N30.AJGgpfekq5Z1f66zpDgiguGXOZShKnUOY2-l-1Iem-g'
 
 // 创建并导出 Supabase 客户端
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
